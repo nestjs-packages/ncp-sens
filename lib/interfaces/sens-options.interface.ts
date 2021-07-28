61de3ffe549b340247eb7c3f25b8ddc95f6781e7
greatSumini <greatSumini@gmail.com>
2021-07-28 21:45:10 +0900

    feat: make SensModuleOptions.secretKey required
    - sens 패키지의 변경점을 반영

--- lib/interfaces/sens-options.interface.ts
+++ lib/interfaces/sens-options.interface.ts
@@ -1,25 +1,10 @@
 import { ModuleMetadata } from '@nestjs/common';
-import {} from '@pickk/sens';
-import {
-  AlimtalkOptions,
-  SensOptions,
-  SmsOptions,
-} from '@pickk/sens/dist/interfaces';
+import { AlimtalkOptions, SensOptions, SmsOptions } from '@pickk/sens';
 
-export type SensModuleOptions = Omit<SensOptions, 'secretKey'> &
-  (
-    | {
-        sms: Omit<SmsOptions, keyof SensOptions>;
-      }
-    | {}
-  ) &
-  (
-    | {
-        secretKey: SensOptions['secretKey'];
-        alimtalk: Omit<AlimtalkOptions, keyof SensOptions>;
-      }
-    | {}
-  );
+export type SensModuleOptions = SensOptions & {
+  sms?: Omit<SmsOptions, keyof SensOptions>;
+  alimtalk?: Omit<AlimtalkOptions, keyof SensOptions>;
+};
 
 export interface SensModuleAsyncOptions
   extends Pick<ModuleMetadata, 'imports'> {
